b4413c59e2ea6985dac01d166e2241047e5b9589
Luis Cedano <king.romanus1@gmail.com>
2015-05-12 11:09:14 -0700

    Milestone 5 Complete

--- index.php
+++ index.php
@@ -5,18 +5,21 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using define.
-define('client_id', 'a8d1c01a9eb14ad0b9abf3a4e384ccee');
+define('clientID', 'a8d1c01a9eb14ad0b9abf3a4e384ccee');
 define('client_secret', '976c493b03464f358c136a20015503d5');
-define('redirectURL', 'http://localhost:8888/LuisApi/index.php');
+define('redirectURI', 'http://localhost:8888/LuisApi/index.php');
 define('ImageDirectory', 'pics/');
 ?>
 
-<!--CLIENT INFO
-CLIENT ID
-a8d1c01a9eb14ad0b9abf3a4e384ccee
-CLIENT SECRET
-976c493b03464f358c136a20015503d5
-WEBSITE URL
-http://localhost:8888/LuisApi/index.php
-REDIRECT URI
-http://localhost:8888/LuisApi/index.php -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <!-- Creating a Login for people to go and give approval for our web app to access their Instagram Account
+    After getting approval we are now going to habe the information so that we can play with it.
+    -->
+    <a href="https:api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI?>&response_type=code">LOGIN</a>
+  </body>
+</html>
