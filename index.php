0f2841b5e3c13f2f417d995f6270ddd8c376d0ac
Luis Cedano <king.romanus1@gmail.com>
2015-05-13 18:52:37 -0700

    Milestones 6,7,8 Complete

--- index.php
+++ index.php
@@ -7,8 +7,32 @@ session_start();
 //Make Constants using define.
 define('clientID', 'a8d1c01a9eb14ad0b9abf3a4e384ccee');
 define('client_secret', '976c493b03464f358c136a20015503d5');
-define('redirectURI', 'http://localhost:8888/LuisApi/index.php');
+define('redirectURI', 'http://localhost/LuisApi/index.php');
 define('ImageDirectory', 'pics/');
+
+
+if (isset($_GET['code'])){
+  $code = ($_GET['code']);
+  $url = 'https://api.instagram.com/oauth/access_token';
+  $access_token_settings = array('client_id' => clientID,
+                                  'client_secret' => client_secret,
+                                  'grant_type' => 'authorization_code',
+                                  'redirect_uri' => redirectURI,
+                                  'code' => $code
+                                  );
+//cURL is what we use in PHP, it's a library calls to other API'S
+$curl = curl_init($url); //setting a cURL session and we put in $url because that's where we're getting the date from
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); //setting the POSTFIELDS to the array setup that we created
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);//setting it equal to 1 because we are getting strings back.
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);//but in live work-production, we want to set this to true.
+
+$result = curl_exec($curl);
+curl_close($curl);
+
+$result = json_decode($result, true);
+echo $result['user']['username'];
+}
 ?>
 
 <!DOCTYPE html>
