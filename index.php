bce13a3077da7add0a8b1147a9cba381ad54e48b
Luis Cedano <king.romanus1@gmail.com>
2015-05-14 10:50:18 -0700

    Fixed Error from Milestone 9

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ define('ImageDirectory', 'pics/');
 function connectionToInstagram($url){
     $ch = curl_init();
 
-    curl_setopt_array($ch, array{
-        CURLOPT_URL => $url;
-        CURLOPT_RETURNTRANSFER => true;
-        CURLOPT_SSL_VERIFYPEER => false;
-        CURLOPT_SSL_VERIFYHOST => 2;
-    });
+    curl_setopt_array($ch, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => 2,
+    ));
     $result = curl_exec($ch);
     curl_close($ch);
     return $result;
